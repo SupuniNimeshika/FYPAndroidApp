1c570b0d0bb9683368b27884d8752b73150aab83
Supuni Nimeshika <Supuni.Nimeshika@gmail.com>
2018-12-25 08:49:29 +0530

    Add API Call To Project Through Button

--- app/src/main/java/com/example/supuninimeshika/fypmooddetection/MainActivity.java
+++ app/src/main/java/com/example/supuninimeshika/fypmooddetection/MainActivity.java
@@ -10,9 +10,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -22,6 +30,7 @@ import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.squareup.picasso.Picasso;
+
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -38,6 +47,9 @@ public class MainActivity extends AppCompatActivity {
     TextView txtEmail, txtBirthday, txtFriends;
     ProgressDialog mDialog;
     ImageView imgAvatar;
+    Button button;
+    TextView textView;
+    String server_url ="http://192.168.8.101/5000/accessToken/supuni";
 
     //Press ctrl + O
 
@@ -46,6 +58,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode,resultCode,data);
+        button =(Button)findViewById(R.id.btn);
+        textView =(TextView)findViewById(R.id.txtResponse);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                textView.setText("sadeepa");
+
+                final RequestQueue requestQueue =Volley.newRequestQueue(MainActivity.this);
+
+                StringRequest stringRequest =new StringRequest(Request.Method.POST, server_url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+
+                                textView.setText(response);
+                                requestQueue.stop();
+
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        textView.setText("Something went wrong....");
+                        error.printStackTrace();
+                        requestQueue.stop();
+                    }
+                });
+                requestQueue.add(stringRequest);
+
+            }
+        });
     }
 
     @Override
